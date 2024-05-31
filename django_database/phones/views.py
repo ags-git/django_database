7b09ac57f35895d03bd8436bf99f4bd8bd997281
Aleksandr Shalamov <ags-mail@yandex.ru>
2024-05-31 21:41:00 +0500

    Добавлена возможность сортировки

--- django_database/phones/views.py
+++ django_database/phones/views.py
@@ -9,7 +9,15 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    phones = Phone.objects.all()
+    sorting = {'sort=name': 'name',
+               'sort=min_price': 'price',
+               'sort=max_price': '-price'}
+
+    if len(request.META['QUERY_STRING']) > 0:
+        phones = Phone.objects.order_by(sorting[request.META['QUERY_STRING']])
+    else:
+        phones = Phone.objects.all()
+
     context = {'phones': phones}
     return render(request, template, context)
 
